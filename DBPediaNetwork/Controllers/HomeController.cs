d49d84b25335022b2bcfc680e1f1dc8e446da04a
airtonsrsena <airton_serra_sena@hotmail.com>
2022-04-18 15:11:00 -0300

    Primeiro Commit.

---
+++ DBPediaNetwork/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using DBPediaNetwork.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using VDS.RDF;
+using VDS.RDF.Query;
+
+namespace DBPediaNetwork.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public ActionResult Search(string pesquisa)
+        {
+            //Define a remote endpoint
+            //Use the DBPedia SPARQL endpoint with the default Graph set to DBPedia
+            SparqlRemoteEndpoint endpoint = new SparqlRemoteEndpoint(new Uri("http://dbpedia.org/sparql"), "http://dbpedia.org");
+
+
+            string query = "SELECT  ?predicate ?object WHERE { dbr:" + pesquisa + " ?predicate ?object} LIMIT 5";
+
+            List<string> resultString = new List<string>();
+
+
+
+            //Make a SELECT query against the Endpoint
+            SparqlResultSet results = endpoint.QueryWithResultSet(query);
+            foreach (SparqlResult result in results)
+            {
+                resultString.Add(result.ToString());
+            }
+
+            return Json(resultString);
+        }
+    }
+}
---
+++ DBPediaNetwork/wwwroot/js/index.js
@@ -0,0 +1,78 @@
+﻿var appIndex = {
+    init: function () {
+        //appIndex.constroiGrafico();
+        appIndex.searchFunction();
+    },
+    constroiGrafico: () => {
+        const nodes = [
+            { id: 1, label: "UFBA" },
+            { id: 2, label: "IC" },
+            { id: 3, label: "DCC" },
+            { id: 4, label: "Airton" },
+            { id: 5, label: "Fred" },
+            { id: 6, label: "João Carlos Salles" },
+        ];
+
+        const edges = [
+            { from: 1, to: 2 },
+            { from: 1, to: 6 },
+            { from: 2, to: 3 },
+            { from: 3, to: 4 },
+            { from: 3, to: 5 },
+        ];
+
+        // create a network
+        var container = document.getElementById("mynetwork");
+        var data = {
+            nodes: nodes,
+            edges: edges,
+        };
+        var options = {};
+        var network = new vis.Network(container, data, options);
+    },
+    searchFunction: () => {
+        $("#btnSearch").click(function () {
+            var pesquisa = $("#inpSearch").val().trim();
+
+            const nodes = [];
+            nodes.push({ id: 1, label: pesquisa });
+
+            const edges = [];
+
+            $.post("Home/Search",
+                {
+                    pesquisa: pesquisa
+                },
+                function (result) {
+                    for (i = 0; i < result.length; i++) {
+                        let id = (i + 2);
+                        let label = result[i].split("?object = ")[1];
+                        let node = { id: id, label: label }
+
+                        nodes.push(node);
+
+                        edges.push({ from: 1, to: id });
+                    }
+
+                    // create a network
+                    var container = document.getElementById("mynetwork");
+                    var data = {
+                        nodes: nodes,
+                        edges: edges,
+                    };
+                    var options = {};
+                    var network = new vis.Network(container, data, options);
+                })
+                .fail(function (result) {
+                    console.log("Ocorreu um erro ao pesquisar.")
+                });
+
+        });
+    }
+
+};
+
+
+(function () {
+    appIndex.init();
+})();
